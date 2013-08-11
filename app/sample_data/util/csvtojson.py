d3471e53bae501b65a06645f18cd34729b216efb
Kent Chen <chkche1@gmail.com>
2013-08-11 15:59:56 -0700

    Finished a very rough stacked bar chart. Needs refinement

--- app/sample_data/util/csvtojson.py
+++ app/sample_data/util/csvtojson.py
@@ -1,14 +1,14 @@
 import csv
 import json
 import sys
-
+import os.path
 
 if __name__ == "__main__":
     if len(sys.argv) !=2:
         print "Usage: python csvtojson.py filename"
         sys.exit(0)
     filename = sys.argv[1]
-    name = filename[:filename.find('.')]+'.json'
+    name = os.path.splitext(os.path.basename(filename))[0]+'json'
     with open(filename,'r') as f:
         reader = csv.DictReader(f)
         with open(name,'w') as fw:
