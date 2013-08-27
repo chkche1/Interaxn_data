e132aeda143aa2e174c042ddc02352e5be9f1307
Ben Shyong <likeachild@gmail.com>
2013-08-26 23:24:02 -0400

    basis parser

---
+++ app/sample_data/util/basis_dump.py
@@ -0,0 +1,41 @@
+import sys
+import os.path
+import urllib
+import datetime
+import time
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print "Usage: python basis_dump.py basisID start_date(YYYY-MM-DD) end_date"
+        sys.exit(0)
+    # basis_id = sys.argv[1]
+    basis_id = "50e518d391d730482d000d8c"
+    start_date_string = map(int, sys.argv[2].split('-'))
+    start_date = datetime.date(start_date_string[0], start_date_string[1], start_date_string[2])
+    end_date_string = map(int, sys.argv[3].split("-"))
+    end_date = datetime.date(end_date_string[0], end_date_string[1], end_date_string[2])
+
+
+    for single_date in (start_date + datetime.timedelta(n) for n in range((end_date-start_date).days)):
+        filename = (basis_id +
+                    "_" +
+                    time.strftime("%Y-%m-%d", single_date.timetuple()) +
+                    ".json")
+        data = urllib.urlopen("https://app.mybasis.com/api/v1/chart/" + basis_id + ".json?start_date=" + \
+          "2013-08-26" + \
+          "&summary=true \
+          &interval=60 \
+          &units=ms \
+          &start_offset=0 \
+          &end_offset=0 \
+          &heartrate=true \
+          &steps=true \
+          &calories=true \
+          &gsr=true \
+          &skin_temp=true \
+          &air_temp=true \
+          &bodystates=true")
+        with open(filename, 'w') as f:
+            f.write(data.read())
+
+
