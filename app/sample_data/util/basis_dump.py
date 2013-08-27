ba36decff111fcf8a6e13dda8d8698195b9d228e
Ben Shyong <likeachild@gmail.com>
2013-08-26 23:40:49 -0400

    dump basis data for ben

--- app/sample_data/util/basis_dump.py
+++ app/sample_data/util/basis_dump.py
@@ -1,3 +1,10 @@
+# This python script fetches Basis B1 band data
+# from the Basis website and saves the data as
+# JSON files.
+#
+# Created August 26, 2013 by Benjamin Shyong.
+# hello@benshyong.com
+
 import sys
 import os.path
 import urllib
@@ -8,19 +15,19 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print "Usage: python basis_dump.py basisID start_date(YYYY-MM-DD) end_date"
         sys.exit(0)
-    # basis_id = sys.argv[1]
-    basis_id = "50e518d391d730482d000d8c"
+    basis_id = sys.argv[1]
     start_date_string = map(int, sys.argv[2].split('-'))
     start_date = datetime.date(start_date_string[0], start_date_string[1], start_date_string[2])
     end_date_string = map(int, sys.argv[3].split("-"))
     end_date = datetime.date(end_date_string[0], end_date_string[1], end_date_string[2])
 
 
-    for single_date in (start_date + datetime.timedelta(n) for n in range((end_date-start_date).days)):
+    for single_date in (start_date + datetime.timedelta(n) for n in range((end_date-start_date).days+1)):
         filename = (basis_id +
                     "_" +
                     time.strftime("%Y-%m-%d", single_date.timetuple()) +
                     ".json")
+        print "fetching data for " + time.strftime("%Y-%m-%d", single_date.timetuple())
         data = urllib.urlopen("https://app.mybasis.com/api/v1/chart/" + basis_id + ".json?start_date=" + \
           "2013-08-26" + \
           "&summary=true \
@@ -37,5 +44,6 @@ if __name__ == "__main__":
           &bodystates=true")
         with open(filename, 'w') as f:
             f.write(data.read())
+        print "wrote to " + filename
 
 
